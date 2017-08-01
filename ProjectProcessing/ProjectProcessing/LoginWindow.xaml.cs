ea0841f1060c321ba91dc685e0cb8a592d356719
jessethings <jessethings@gmail.com>
2017-08-01 22:34:18 +1200

    Design Concept

---
+++ ProjectProcessing/ProjectProcessing/LoginWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace ProjectProcessing
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainApp ma = new MainApp();
+            ma.Owner = GetWindow(this);
+            Hide();
+            ma.ShowDialog();
+            Close();
+        }
+    }
+}
