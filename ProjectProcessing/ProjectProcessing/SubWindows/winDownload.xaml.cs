db7f4a687278b35dbedd07b69348747b4949d49c
jessethings <jessethings@gmail.com>
2017-11-03 00:39:08 +0900

    additional changes

--- ProjectProcessing/ProjectProcessing/SubWindows/winDownload.xaml.cs
+++ ProjectProcessing/ProjectProcessing/SubWindows/winDownload.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using FortunaExcelProcessing.Objects;
 
 namespace ProjectProcessing.SubWindows
 {
@@ -27,6 +28,32 @@ namespace ProjectProcessing.SubWindows
         private void butCancel_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadCurrentDates();
+        }
+
+        //get all the dates for each of the weekly data uploads on the server
+        private void LoadCurrentDates()
+        {
+            cmboDates.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(delegate () //send a request to the original thread to do the following operation
+            {
+                List<DateHolder> dates = DownloadData.GetUserWeeklyDataDates();
+
+                for (int i = 0; i < dates.Count; i++)
+                {
+                    cmboDates.Items.Add(dates[i].sdate);
+                }
+
+                cmboDates.Tag = dates;
+            }));
+        }
+
+        private void butGenerateReport_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
